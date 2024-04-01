d25de9819909b5eec74a365252c10ec22d8c6a2d
Henry X <henryxia9999@gmail.com>
2024-03-31 23:33:19 -0500

    dsu works :o

---
+++ data_structures/union_find.h
@@ -0,0 +1,34 @@
+/** Disjoint Set Union / Union-Find
+ * source: https://github.com/chenvictor/acm/blob/master/ds/DSU.cpp
+ * does union by size, where size is stored in the root
+ * USAGE
+ *  find(x) -> int; gives the root of the group containing x
+ *  size(x) -> int; gives the size of the group containing x
+ *  link(x) -> bool; link x and y, returns true if new link
+ *  size() -> int; gives the number of groups
+ * STATUS
+ *  tested: boj/3108
+ */
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+struct union_find {
+  std::vector<int> r;
+  int_fast32_t _size;
+  union_find(size_t n) : r(n, -1), _size(n) {}
+  auto find(int x) -> int { return r[x] < 0 ? x : r[x] = find(r[x]); }
+  auto size(int x) -> int { return -r[find(x)]; }
+  auto link(int x, int y) -> bool {
+    if ((x = find(x)) == (y = find(y))) return false;
+    if (r[x] < r[y]) std::swap(x, y);
+    r[y] += r[x];
+    r[x] = y;
+    _size -= 1;
+    return true;
+  }
+  auto size() -> int { return (int)_size; }
+};
+
+using DSU = union_find;
