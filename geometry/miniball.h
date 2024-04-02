525d3531ef55cbc34545ed622a791132471ab36f
Henry X <henryxia9999@gmail.com>
2024-04-02 02:45:34 -0500

    miniball
    adapted from https://people.inf.ethz.ch/gaertner/subdir/software/miniball.html

---
+++ geometry/miniball.h
@@ -0,0 +1,131 @@
+/** Minimum Enclosing Ball
+ *  source: Fast and Robust Smallest Enclosing Balls (Bernd Gaertner)
+ *  https://people.inf.ethz.ch/gaertner/subdir/texts/own_work/esa99_final.pdf
+ *  https://people.inf.ethz.ch/gaertner/subdir/software/miniball.html
+ * USAGE
+ *  idk
+ * STATUS
+ *  tested: boj/11930
+ *  TODO: https://www.acmicpc.net/problemset?sort=ac_desc&algo=162
+ */
+#pragma once
+
+#include "geometry/point_nd.h"
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <list>
+
+template <std::floating_point T, unsigned Dimension>
+  requires(Dimension > 0)
+struct miniball {
+  using point_t = point_nd<T, Dimension>;
+
+  std::array<point_t, Dimension + 1> c = {};  // centers
+  std::array<T, Dimension + 1> r2 = {T(-1)};  // squared radii
+
+  std::array<point_t, Dimension + 1> v;                   // helper
+  std::array<T, Dimension + 1> z;                         // helper
+  std::array<T, Dimension + 1> f;                         // helper
+  std::array<std::array<T, Dimension>, Dimension + 1> A;  // helper
+
+  std::list<unsigned> L;   // list of support points
+  unsigned n_forced = 0;   // number of forced points
+  unsigned n_support = 0;  // number of support points
+  unsigned last_i = 0;     // current center and radius
+
+  std::vector<point_t> const ps;  // all points
+
+  auto center() const -> point_t { return c[last_i]; }
+  auto radius() const -> T { return std::sqrt(r2[last_i]); }
+  auto squared_radius() const -> T { return r2[last_i]; }
+
+  template <std::input_iterator InputIt>
+  miniball(InputIt first, InputIt last) : ps(first, last) {
+    build();
+  }
+  template <typename Container>
+    requires(requires(Container const& cont) {
+      std::begin(cont);
+      std::end(cont);
+    })
+  miniball(Container const& cont) : miniball(std::begin(cont), std::end(cont)) {}
+
+  auto build() -> void {
+    auto support_end = L.begin();  // L .. support_end is at most (Dimension + 1)
+    unsigned stale = 0;
+    do {
+      auto const old_r2 = squared_radius();
+      auto max_e = T(0);
+      unsigned pivot = 0;
+      for (unsigned k = 0; k < ps.size(); k++) {
+        auto const e = norm(ps[k] - center()) - old_r2;
+        if (e > max_e) {
+          max_e = e;
+          pivot = k;
+        }
+      }
+
+      if (old_r2 < 0 || max_e > 0) {
+        if (std::find(L.begin(), support_end, pivot) == support_end) {
+          if (push(pivot)) {
+            support_end = move_to_front_mb(support_end);
+            pop();
+            L.push_front(pivot);
+            if (std::distance(L.begin(), support_end) == Dimension + 2) support_end--;
+          }
+        }
+      }
+      if (squared_radius() > old_r2) stale = 0;
+      else stale++;
+    } while (stale < 2);
+    // TODO is this stale counter necessary or can I just check latest_r2 > old_r2?
+  }
+
+  auto move_to_front_mb(std::list<unsigned>::iterator n) -> std::list<unsigned>::iterator {
+    auto support_end = L.begin();
+    if (n_forced == Dimension + 1) return support_end;
+    for (auto i = L.begin(); i != n;) {
+      auto const j = i++;
+      if (norm(ps[*j] - center()) - squared_radius() > 0) {
+        if (push(*j)) {
+          support_end = move_to_front_mb(j);
+          pop();
+          if (support_end == j) support_end++;
+          L.splice(L.begin(), L, j);
+        }
+      }
+    }
+    return support_end;
+  }
+
+  auto pop() -> void { --n_forced; }
+  auto push(unsigned it) -> bool {
+    static constexpr auto eps =
+        std::numeric_limits<T>::epsilon() * std::numeric_limits<T>::epsilon();
+    if (n_forced == 0) {
+      c[0] = ps[it];
+      r2[0] = 0;
+    } else {
+      v[n_forced] = ps[it] - c[0];
+      for (unsigned i = 1; i < n_forced; i++) {
+        A[n_forced][i] = v[n_forced].dot(v[i]) * 2 / z[i];
+      }
+      for (unsigned i = 1; i < n_forced; i++) {
+        v[n_forced] -= v[i] * A[n_forced][i];
+      }
+      z[n_forced] = norm(v[n_forced]) * 2;
+      if (z[n_forced] < eps * squared_radius()) {
+        return false;
+      }
+      auto const e = norm(ps[it] - c[n_forced - 1]) - r2[n_forced - 1];
+      f[n_forced] = e / z[n_forced];
+      c[n_forced] = c[n_forced - 1] + v[n_forced] * f[n_forced];
+      r2[n_forced] = r2[n_forced - 1] + e * f[n_forced] / 2;
+    }
+    last_i = n_forced;
+    n_support = ++n_forced;
+    return true;
+  }
+};
---
+++ geometry/point_nd.h
@@ -0,0 +1,80 @@
+/* Point
+ * USAGE
+ *  point<T, D>(...);
+ *  T must be integral or floating
+ * STATUS
+ *  untested
+ */
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <iostream>
+
+template <typename T, size_t Dimension>
+  requires(std::is_floating_point_v<T> or std::is_integral_v<T>)
+struct point_nd {
+  std::array<T, Dimension> xs = {};
+  point_nd() = default;
+  template <typename... Args>
+  point_nd(Args... coords) : xs{coords...} {}
+  template <typename U>
+  explicit point_nd(point_nd<U, Dimension> const& other)
+      : point_nd(other, std::make_index_sequence<Dimension>()) {}
+  template <typename U, size_t... Is>
+  explicit point_nd(point_nd<U, Dimension> const& other, std::index_sequence<Is...>)
+      : xs{T(other[Is])...} {}
+  auto operator[](size_t i) -> T& { return xs[i]; }
+  auto operator[](size_t i) const -> T { return xs[i]; }
+  auto operator+(point_nd const& o) const -> point_nd { return point_nd(*this) += o; }
+  auto operator-(point_nd const& o) const -> point_nd { return point_nd(*this) -= o; }
+  auto operator*(T c) const -> point_nd { return point_nd(*this) *= c; }
+  auto operator/(T c) const -> point_nd { return point_nd(*this) /= c; }
+  auto operator-() const -> point_nd { return point_nd(*this).negate(); }
+  auto operator+=(point_nd const& o) -> point_nd& {
+    for (size_t i = 0; i < xs.size(); i++) xs[i] += o.xs[i];
+    return *this;
+  }
+  auto operator-=(point_nd const& o) -> point_nd& {
+    for (size_t i = 0; i < xs.size(); i++) xs[i] -= o.xs[i];
+    return *this;
+  }
+  auto operator*=(T c) -> point_nd& {
+    for (size_t i = 0; i < xs.size(); i++) xs[i] *= c;
+    return *this;
+  }
+  auto operator/=(T c) -> point_nd& {
+    for (size_t i = 0; i < xs.size(); i++) xs[i] /= c;
+    return *this;
+  }
+  auto negate() -> point_nd& {
+    for (size_t i = 0; i < xs.size(); i++) xs[i] = -xs[i];
+    return *this;
+  }
+  auto dot(point_nd const& other) const -> T {
+    T res = 0;
+    for (size_t i = 0; i < xs.size(); i++) {
+      res += xs[i] * other.xs[i];
+    }
+    return res;
+  }
+  auto norm() const -> T { return dot(*this); }
+  friend auto operator<<(std::ostream& os, point_nd const& v) -> std::ostream& {
+    os << "(" << v[0];
+    for (size_t i = 1; i < Dimension; i++) {
+      os << "," << v[i];
+    }
+    return os << ")";
+  }
+  friend auto operator>>(std::istream& is, point_nd& v) -> std::istream& {
+    for (size_t i = 0; i < Dimension; i++) {
+      is >> v[i];
+    }
+    return is;
+  }
+};
+
+template <typename T, size_t D>
+auto norm(point_nd<T, D> const& v) -> T {
+  return v.norm();
+}
